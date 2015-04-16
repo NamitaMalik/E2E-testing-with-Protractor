f308f46cc33e905b3bc1c22d1e169682ed7967a8
Namita Malik <namita.malik@paytm.com>
2015-04-16 15:24:59 +0530

    Adding field length validation test

--- test/spec.js
+++ test/spec.js
@@ -11,26 +11,32 @@
         it('should have correct title', function () {
             expect(browser.getTitle()).toEqual('Credit Card');
         });
-        it('input box should be disabled',function(){
+        it('keeps the input field disabled',function(){
            expect(element(by.id('hasCard')).isEnabled()).toBe(false);
         });
-        it('input box should enable when checkbox is checked',function(){
+        it('enables the input field',function(){
             element(by.model('data.checkCard')).click();
             expect(element(by.id('hasCard')).isEnabled()).toBe(true);
         });
-        it('error message should be shown when non numeric characters are written in input',function(){
+        it('gives an error message on writing invalid credit card number',function(){
             element(by.model('data.checkCard')).click();
             element(by.model('data.cardNumber')).sendKeys("abcdefghijkikiki");
             element(by.id('save')).click();
             expect(element(by.binding('errorMessage')).getText()).toEqual("Please enter a valid credit card number");
         });
-        it('success message should be shown when valid credit card number is added',function(){
+        it('gives a success message on writing a valid credit card number',function(){
             var cardNumber = "1234567899009876";
             element(by.model('data.checkCard')).click();
             element(by.model('data.cardNumber')).sendKeys(cardNumber);
             element(by.id('save')).click();
             var textToCheck =
             expect(element(by.binding('successMessage')).getText()).toEqual("Your credit card number" + " " + cardNumber + " has been saved with us.");
+        });
+        it('gives an error message when credit card number entered is less than 16 digits',function(){
+            element(by.model('data.checkCard')).click();
+            element(by.model('data.cardNumber')).sendKeys("1234567890");
+            element(by.id('save')).click();
+            expect(element(by.binding('errorMessage')).getText()).toEqual("Please enter a valid credit card number");
         });
     });
 })();
